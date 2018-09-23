49b55c38592125b8573ce084c09c0a3460b09955
manavdewan1990 <manavdewan1990@gmail.com>
2018-09-24 00:44:55 +0530

    Update node-app.tf

--- node-app.tf
+++ node-app.tf
@@ -8,13 +8,6 @@ provider "aws" {
   profile =    "default"
 }
 
-terraform {
-  backend "s3" {
-    bucket="manavd-apps"
-    key="terraform/dev/terraform_dev.tfstate"
-    region="ap-south-1"
-  }
-}
 
 data "aws_ami" "node_app_ami" {
   most_recent = true
